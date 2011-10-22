02e063f423a663c6f51bc2bff99d53ecd8d3d3d1
Ryan Montgomery <rmontgomery429@gmail.com>
2011-10-22 11:20:00 -0400

    Chapters * added basic chapter CRUDing

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,9 +1,41 @@
 class ChaptersController < ApplicationController
   before_filter :authenticate_user!
-
-  respond_to :json
+  
+  respond_to :json, :html
   
   def index
-    respond_with(@chapters = Chapter.where("name like '%#{params[:q]}%'"))
+    if params[:q]
+      respond_with(@chapters = Chapter.where("title like '%#{params[:q]}%'"))
+    end
+    
+    respond_with(@chapters = Chapter.all)
+  end
+  
+  def new
+    @chapter = Chapter.new
+  end
+  
+  def edit
+    @chapter = Chapter.find(params[:id])
+  end
+  
+  def update
+    @chapter = Chapter.find(params[:id])
+    
+    if @chapter.update_attributes(params[:chapter])
+      redirect_to chapters_path, :notice => "Updated successully!"
+    else
+      render action: "edit"
+    end
+  end
+  
+  def create
+    @chapter = Chapter.new(params[:chapter])
+    
+    if @chapter.save
+      redirect_to chapters_path, :notice => "Created successully!"
+    else
+      render action: "new"
+    end
   end
 end
