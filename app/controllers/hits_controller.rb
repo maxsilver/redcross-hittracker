4daaebdc0716c232a665d55560c09e939b7a9f39
Matthew Seeley <matthew@threadlight.com>
2011-10-22 14:40:34 -0400

    Fixes for new hit form

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -1,14 +1,18 @@
 class HitsController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def index
     @hits = Hit.all
   end
-  
+
   def new
     @hit = Hit.new()
+    @media_outlets = MediaOutlet.all
+    @reporters = Reporter.all
+    @chapters = Chapter.all
+    @press_releases = PressRelease.all
   end
-  
+
   def edit
     @hit = Hit.find(params[:id])
     @media_outlets = MediaOutlet.all
@@ -16,17 +20,17 @@ class HitsController < ApplicationController
     @chapters = Chapter.all
     @press_releases = PressRelease.all
   end
-  
+
   def update
     @hit = Hit.find(params[:id])
-    
+
     if @hit.update_attributes(params[:hit])
       redirect_to hits_path, :notice => "Updated successully!"
     else
       render action: "edit"
     end
   end
-  
+
   def create
     @hit = current_user.hits.build(params[:hit])
     if @hit.save
@@ -35,5 +39,5 @@ class HitsController < ApplicationController
       render :new
     end
   end
-    
+
 end
