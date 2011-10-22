aec92eb6491a89f53f1f9e34a32082f3f945d01f
Ryan Montgomery <rmontgomery429@gmail.com>
2011-10-22 14:22:05 -0400

    Users * creating suers moved to it's own page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
   
+  def new
+    @user = User.new
+  end
+  
+  
   def create
     @user = User.new(params[:user])
     if @user.save
