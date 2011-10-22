5d58ccf1576edc7eb85f504c23fce3c63e1b7fd7
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-10-21 20:03:20 -0400

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'pg'
+gem 'thin'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'heroku'
+  gem 'foreman'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+group :test do
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HitTracker::Application.config.secret_token = 'be647d5100e2a1d84e24ea69a329164b92177b4b903324b50b302fb1942c6007e69c668742d3d0737a248a275eeabc1eb99d40c80b77c767697459238214e565'
