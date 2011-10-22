365e77a9907c722dddd7f297cd06ee72ba47a75f
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-10-21 22:14:10 -0400

    * More form work * jQuery TokenInput for autocomplete fields * jQuery UI date picker for date fields * jQuery UI theme

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -2,6 +2,9 @@ class HitsController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    
+  end
+  
+  def create
+    redirect_to hits_path
   end
 end
---
+++ app/controllers/outlets_controller.rb
@@ -0,0 +1,6 @@
+class OutletsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+  end
+end
