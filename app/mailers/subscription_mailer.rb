6cc07c3a97d6f3530205bc9dc353e3837e57671c
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-10-23 00:27:18 -0400

    updating from email

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,5 +1,5 @@
 class SubscriptionMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "media-hit-tracker@ggr.redcross.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
